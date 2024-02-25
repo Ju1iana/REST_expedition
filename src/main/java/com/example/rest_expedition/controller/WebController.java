c2a887bad3cbf4b4928946bda3fdbac830c17618
tuchka <u.pankratova@internet.ru>
2024-02-25 13:26:58 +0300

    check

--- src/main/java/com/example/rest_expedition/controller/WebController.java
+++ src/main/java/com/example/rest_expedition/controller/WebController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:8080")
 public class WebController {
 
   @GetMapping("/index")
-  public String indexPage() {
+  public String index() {
     return "index";
   }
 }
