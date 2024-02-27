1d5512670726091b11d32558ebb9336d7450daee
Savara <Greysaboter@gmail.com>
2024-02-27 22:18:17 +0300

    Input update
    Оптимизировал два скрипта в один postresp.js + postquest.js
    Оптимизорвал две кнопки в одну PostResponse + ButtonPost
    Сделал отправку заполненных полей человека в БД (Create Person)

--- src/main/resources/static/js/postquest.js
+++ src/main/resources/static/js/postquest.js
@@ -1,22 +1,24 @@
-const ButtonPost = document.getElementById('fetchButtonPost');
 
-var gender = document.getAttribute('genders')
-var ages = document.getAttribute('ages')
-var heightes = document.getAttribute('heightes')
-var weightes = document.getAttribute('weightes')
-var fios = document.getAttribute('fios')
+const ButtonPoste = document.getElementById('fetchButtonPost');
 
-
-
-ButtonPost.addEventListener('click', () => {
+ButtonPoste.addEventListener('click', () => {
 
     async function postResponse() {
+        var inputElement1 = document.getElementById("fios");
+        var inputElement2 = document.getElementById("genders");
+        var inputElement3 = document.getElementById("ages");
+        var inputElement4 = document.getElementById("heightes");
+        var inputElement5 = document.getElementById("weightes");
+        var value1 = inputElement1.value;
+        var value2 = inputElement2.value;
+        var value3 = inputElement3.value;
+        var value4 = inputElement4.value;
+        var value5 = inputElement5.value;
+
+        console.log(value1, value2, value3, value4, value5);
+
         var user = {
-            personGender: gender,
-            age: ages,
-            h: heightes,
-            weight: weightes,
-            fio: fios
+            personGender: value2, age: value3, h: value4, weight: value5 , fio: value1
         };
 
         let response = await fetch('http://localhost:8080/addPerson', {
--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -13,37 +13,7 @@ Buttonget.addEventListener('click', () => {
         document.getElementById('getContent').innerText = JSON.stringify(content, null, 2);
 
 
-
     }
     getResponse()
 })
 
-
-
-
-
-//     let response = await fetch('http://localhost:8080/getAllPeople', {
-//          method: 'GET',
-//         withCredentials: true,
-//          crossorigin: true,
-//          mode: 'no-cors',             // ПРОБЛЕМА В NO-CORS - НАДО МЕНЯТЬ ЗАГОЛОВОК НА СЕРВЕРЕ ЛИБО ЧЁТ ДУМАТЬ!
-//      })
-//     let content = await response.json()
-//     console.log(content)
-
-
-//     let content = await response.text()
-//     console.log(content)
-
-
-//async function getResponse() {
-//      let response = await fetch('http://localhost:8080/getAllPeople', {
-//          method: 'GET',
-//         withCredentials: true,
-//          crossorigin: true,
-//          mode: 'no-cors',
-//      })
-//     console.log(JSON.parse(response))
-//
-//  }
-//  getResponse()
