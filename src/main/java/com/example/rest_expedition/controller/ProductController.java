8c4b59cef76b0d41d3a58bcc69d72a4f07703ef3
tuchka <u.pankratova@internet.ru>
2024-03-25 19:40:44 +0300

    Added FIO in the output of hiking calories
    - change the logic in the future (!)
    - check data changes and user deletions (!)

--- src/main/java/com/example/rest_expedition/controller/ProductController.java
+++ src/main/java/com/example/rest_expedition/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.rest_expedition.controller;
 
 import com.example.rest_expedition.dto.ProductDTO;
 import com.example.rest_expedition.model.Ration;
+import com.example.rest_expedition.service.PeopleService;
 import com.example.rest_expedition.service.PersonCalculator;
 import com.example.rest_expedition.model.Product;
 import com.example.rest_expedition.service.ProductCalculator;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,10 +24,12 @@ public class ProductController {
   private final PersonCalculator personCalculator;
   private final ProductCalculator productCalculator;
   private final ProductService productService;
+  private final PeopleService peopleService;
   private final ModelMapper mapper;
 
   @Autowired
-  public ProductController(PersonCalculator personCalculator, ProductCalculator calculator, ProductService foodItemService, ModelMapper mapper) {
+  public ProductController(PersonCalculator personCalculator, ProductCalculator calculator, ProductService foodItemService, PeopleService peopleService, ModelMapper mapper) {
+    this.peopleService = peopleService;
     this.mapper = mapper;
     this.productCalculator = calculator;
     this.productService = foodItemService;
@@ -48,9 +53,16 @@ public class ProductController {
 
   @Operation(summary = "List of hiking calories")
   @RequestMapping(method = RequestMethod.GET, value = "/getCalories")
-  public List<Integer> getCalories(){
+  public List<String> getCalories(){
     personCalculator.calcHikingCalories();
-    return productCalculator.initCalories();
+    List<String> result = new ArrayList<>();
+    for (int i = 0; i < productCalculator.initCalories().size(); i++) {
+      result.add(peopleService.getAllPeople().get(i).getFIO() + " - " + productCalculator.initCalories().get(i));
+    }
+
+    return result;
+
+    /*return productCalculator.initCalories();*/
   }
 
   @Operation(summary = "Get all calories")
