be55e8754e0833d8e83aa4049517a25bbdf6b780
Savara <Greysaboter@gmail.com>
2024-04-16 09:38:20 +0300

    Major Table Update
    - Полностью переписана и оптимизированна внутренняя архитектура генерации таблицы.
    - Изменения в CSS таблицы.
    - Исправлены баги связанные с отображением Дня и Заполнения таблицы продуктами.

--- src/main/resources/static/js/hikequest.js
+++ src/main/resources/static/js/hikequest.js
@@ -17,8 +17,19 @@ ButtonPostehike3.addEventListener('click', () => {
         tableContainer.innerHTML = "";
 
 
+        function leftHead() {
+        var row = $("<tr class='headers-left' id='headers-left'/>")
+        $("#DataTable").append(row);
+        row.append($("<th>" + "Тип/День" + "</th>"));
+        row.append($("<th>" + "Завтрак" + "</th>"));
+        row.append($("<th>" + "Перекус" + "</th>"));
+        row.append($("<th>" + "Обед" + "</th>"));
+        row.append($("<th>" + "Ужин" + "</th>"));
+        }
+        leftHead()
+
         let formData = new FormData();        // Формирование body для POST запроса
-        formData.append('duration', value1);
+        formData.append('duration', one);
         formData.append('type', value2);
         formData.append('difficulty', value3)
 
@@ -35,62 +46,47 @@ ButtonPostehike3.addEventListener('click', () => {
             let result = await response.json();
             console.log(result)
 
-            async function getRation() {
 
-                let DataRation = await fetch('http://localhost:8080/getRation', {
-                    method: 'GET'
-                });
-                let data = await DataRation.json();
-                console.log(data);
+            async function getTableRation(){
+                for(let tr = 0; tr < value1; tr++) {
+                    async function getRation() {
 
-
-                function drawDay() {
-                    var t = data.length;
-                    var k = t/4;
-                    console.log(t);
-                    console.log(k);
-
-                    var row = $("<tr class='headers-left' id='headers-left'/>")
-                    $("#DataTable").append(row);
-                    row.append($("<th>" + "Тип/День" + "</th>"));
-                    row.append($("<th>" + "Завтрак" + "</th>"));
-                    row.append($("<th>" + "Перекус" + "</th>"));
-                    row.append($("<th>" + "Обед" + "</th>"));
-                    row.append($("<th>" + "Ужин" + "</th>"));
+                        let DataRation = await fetch('http://localhost:8080/getRation', {
+                            method: 'GET'
+                        });
+                        let data = await DataRation.json();
+                        console.log(data);
 
 
+                        var headays = $("<tr class='staks' id='staks'/>")
+                        $("#DataTable").append(headays);
+                        tr++;
 
-                    var headays = $("<tr class='staks' id='staks'/>")
-                    $("#DataTable").append(headays);
-                    for (var j = 1; j <= k; j++) {
-                        headays.append($("<th>" + "День " + j + "</th>"));
+
+                        for (var j = 0; j < 1; j++) {
+                            headays.append($("<th>" + "День " + tr + "</th>"));
+
+                            for (var i = 0; i < 4; i++) {
+                                drawRow(data[i]);
+                            }
+
+                            function drawRow(data) {
+                                headays.append("<td>" + data["products"].map(it => it.name).join(', ') + "</td>");
+                            }
+                        }
+
+                        // + " " + it.daily_norm_per_person + "гр " + it.calories_per_100g + "ккал"
+
                     }
-
+                    getRation()
                 }
-                drawDay()
+            }
+            getTableRation()
 
-                var t = data.length;
-                for (var i = 0; i < t; i++) {
-                    drawRow(data[i]);
-                }
-                function drawRow(data) {
-                    $("#staks").append("<td>" + data["products"].map(it => it.name).join(', ') + "</td>");
-                }
-
-                // + " " + it.daily_norm_per_person + "гр " + it.calories_per_100g + "ккал"
-
-
-                // for(var x = 0; x < data.length; x++) {
-                //     for(var y = 0; y < data[x].products.length; y++) {
-                //         $('#test').append('<p>' + data[x].day + ', ' + data[x].nameOfMeal + ': ' + data[x].products[y].name + '</span></p>');
-                //     }
-                // }
-
-                }
-                setTimeout(getRation, 300);
             }
         }
         postHikeResponse()
+
 
 
     // Очистка полей ввода после отправки данных о походе
